a636011c4de34a545d5de30586df8c3784f59028
Liam Killion <liam.killion@gmail.com>
2018-02-11 22:04:58 -0500

    getting 200 response w/ empty array from reviewmonkey url

--- app.rb
+++ app.rb
@@ -3,6 +3,9 @@ require 'sinatra/json'
 require 'sinatra/reloader'
 require 'active_record'
 require 'faraday'
+require 'rest-client'
+require 'json'
+require 'byebug'
 
 Dir['models/**/*.rb'].each { |file| require_relative file }
 also_reload 'models/**/*.rb'
@@ -16,9 +19,13 @@ ActiveRecord::Base.establish_connection(
 )
 
 get '/films/:film_id/recommendations' do
-  puts params
+  response = RestClient.get(REVIEW_MONKEY_URL)
+  byebug
+  films = JSON.parse(response)
+  byebug
+  film = films.each {|film| film.film_id===params["film_id"].to_i}
 end
 
 get '/' do
-  puts 'Testing'
+   'Testing'
 end
